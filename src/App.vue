abaf3e4dc24d09af020bf14bd2bbb8b29285a5c7
yannismth <yannis.mthpro@gmail.com>
2024-01-13 01:05:35 +0100

    Partie compétences de la page bientôt finis s'occuper des mockups pour la suite.

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,133 @@
 </script>
 
 <template>
-  <h1 class="">Salut</h1>
+  <div class="container mx-auto h-28">
+    <div class="flex justify-around items-center h-full w-full">
+      <h1 class="text-4xl uppercase font-[Montserrat] font-black">Yannis<span class="text-purple-300">.</span></h1>
+      <ul class="flex font-[Urbanist] uppercase navnul mr-[60px] font-medium">
+        <li>accueil</li>
+        <li>projets</li>
+        <li>à propos</li>
+        <li>contact</li>
+      </ul>
+      <button class="uppercase border p-2 rounded-lg border-black">Prendre contact</button>
+    </div>
+  </div>
+  <main class="w-[100%] h-[822px] text-[#293033]">
+    <img src="../src/assets/bgmain.png" class="absolute -z-10 caca left-[50%] top-[35%]">
+    <div class="container mx-auto h-full">
+      <div class="uppercase flex flex-col text-center font-[urbanist] font-bold gap-[35px] py-[200px]">
+        <h1 class="text-8xl">Un site qualitatif vient</h1>
+        <div class="flex flex-row items-center justify-center gap-x-[23px]">
+          <h1 class="text-8xl"><span class="font-[Playfair] lowercase italic">avec</span> un peu de patience</h1>
+          <img src="../src/assets/pasti.jpg" alt="" class="rounded-full w-[165px] h-[95px]">
+        </div>
+        <div class="flex flex-row items-center justify-center gap-x-[20px]">
+          <p class="font-medium text-2xl">Un freelance créatif <br> qui donne vie aux <span>projets.</span></p>
+          <h1 class="text-8xl">des clients satisfaits</h1>
+        </div>
+      </div>
+    </div>
+  </main>
+  <section class="sw">
+    <div class="w-[105vw] h-24 bg-[#c5f2e7] -rotate-1 mt-32 -ml-2">
+      <div class="thick flex flex-row items-center w-[400vw] h-full gap-x-32 uppercase text-[26px]">
+        <h1 class="">UX & UI Experiences</h1>
+        <img src="https://assets-global.website-files.com/64f83b3ea875da04ea56a07f/650987062931ef73d6d9277c_star.svg" loading="lazy" alt="">
+        <h1 class="">Développeur web</h1>
+        <img src="https://assets-global.website-files.com/64f83b3ea875da04ea56a07f/650987062931ef73d6d9277c_star.svg" loading="lazy" alt="">
+        <h1>Graphisme</h1>
+        <img src="https://assets-global.website-files.com/64f83b3ea875da04ea56a07f/650987062931ef73d6d9277c_star.svg" loading="lazy" alt="">
+        <h1 class="">UX & UI Experiences</h1>
+        <img src="https://assets-global.website-files.com/64f83b3ea875da04ea56a07f/650987062931ef73d6d9277c_star.svg" loading="lazy" alt="">
+        <h1 class="">Développeur web</h1>
+        <img src="https://assets-global.website-files.com/64f83b3ea875da04ea56a07f/650987062931ef73d6d9277c_star.svg" loading="lazy" alt="">
+        <h1>Graphisme</h1>
+        <img src="https://assets-global.website-files.com/64f83b3ea875da04ea56a07f/650987062931ef73d6d9277c_star.svg" loading="lazy" alt="">
+        <h1 class="">UX & UI Experiences</h1>
+        <img src="https://assets-global.website-files.com/64f83b3ea875da04ea56a07f/650987062931ef73d6d9277c_star.svg" loading="lazy" alt="">
+        <h1 class="">Développeur web</h1>
+        <img src="https://assets-global.website-files.com/64f83b3ea875da04ea56a07f/650987062931ef73d6d9277c_star.svg" loading="lazy" alt="">
+        <h1>Graphisme</h1>
+        <img src="https://assets-global.website-files.com/64f83b3ea875da04ea56a07f/650987062931ef73d6d9277c_star.svg" loading="lazy" alt="">
+        <h1 class="">UX & UI Experiences</h1>
+        <img src="https://assets-global.website-files.com/64f83b3ea875da04ea56a07f/650987062931ef73d6d9277c_star.svg" loading="lazy" alt="">
+        <h1 class="">Développeur web</h1>
+        <img src="https://assets-global.website-files.com/64f83b3ea875da04ea56a07f/650987062931ef73d6d9277c_star.svg" loading="lazy" alt="">
+        <h1>Graphisme</h1>
+        <img src="https://assets-global.website-files.com/64f83b3ea875da04ea56a07f/650987062931ef73d6d9277c_star.svg" loading="lazy" alt="">
+        <h1 class="">UX & UI Experiences</h1>
+        <img src="https://assets-global.website-files.com/64f83b3ea875da04ea56a07f/650987062931ef73d6d9277c_star.svg" loading="lazy" alt="">
+        <h1 class="">Développeur web</h1>
+        <img src="https://assets-global.website-files.com/64f83b3ea875da04ea56a07f/650987062931ef73d6d9277c_star.svg" loading="lazy" alt="">
+        <h1>Graphisme</h1>
+        <img src="https://assets-global.website-files.com/64f83b3ea875da04ea56a07f/650987062931ef73d6d9277c_star.svg" loading="lazy" alt="">
+        <h1 class="">UX & UI Experiences</h1>
+        <img src="https://assets-global.website-files.com/64f83b3ea875da04ea56a07f/650987062931ef73d6d9277c_star.svg" loading="lazy" alt="">
+        <h1 class="">Développeur web</h1>
+        <img src="https://assets-global.website-files.com/64f83b3ea875da04ea56a07f/650987062931ef73d6d9277c_star.svg" loading="lazy" alt="">
+        <h1>Graphisme</h1>
+        <img src="https://assets-global.website-files.com/64f83b3ea875da04ea56a07f/650987062931ef73d6d9277c_star.svg" loading="lazy" alt="">
+        <h1 class="">UX & UI Experiences</h1>
+        <img src="https://assets-global.website-files.com/64f83b3ea875da04ea56a07f/650987062931ef73d6d9277c_star.svg" loading="lazy" alt="">
+        <h1 class="">Développeur web</h1>
+        <img src="https://assets-global.website-files.com/64f83b3ea875da04ea56a07f/650987062931ef73d6d9277c_star.svg" loading="lazy" alt="">
+        <h1>Graphisme</h1>
+      </div>
+    </div>
+  </section>
+  <section class="w-full h-[1287px] py-32 my-28">
+    <div class="flex justify-evenly">
+      <div class="flex flex-col gap-y-12">
+        <h1 class="text-4xl uppercase font-medium">Mes compétences</h1>
+        <p class="text-2xl font-regular">Les services complets que je propose<br> en tant que freelance en développement web<br> sont spécifiquement conçus pour répondre<br> à vos besoins uniques.</p>
+        <button class="border rounded-full text-2xl border-black w-5/12 p-2">En savoir plus</button>
+      </div>
+      <div class="grid gap-y-6 font-[Urbanist]">
+        <img src="https://assets-global.website-files.com/64f83b3ea875da04ea56a07f/64fedc32cfc76bcbe2e96ebc_gi1-large.svg" loading="lazy" alt="" class="feature-icon-image">
+        <h1 class="text-3xl">Product Strategy</h1>
+        <p class="text-lg">Nulla vitae elit libero, a pharetra augue id elit non mi porta gravida at eget metus justo. <br> Etiam sem malesuada magna mollis euismod. Donec ullamcorper nulla non metus.</p>
+        <hr>
+        <img src="https://assets-global.website-files.com/64f83b3ea875da04ea56a07f/64fedc0371b5f82b71ed18d1_gi2-large.svg" loading="lazy" alt="" class="feature-icon-image">
+        <h1 class="text-3xl">Identity & Branding</h1>
+        <p class="text-lg">Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed blandit <br> tempus porttitor. Vivamus sagittis lacus vel augue laoreet rutrum faucibus auctor.</p>
+        <hr>
+        <img src="https://assets-global.website-files.com/64f83b3ea875da04ea56a07f/64fedc05d795d0489da3183d_gi3-large.svg" loading="lazy" alt="" class="feature-icon-image">
+        <h1 class="text-3xl">Digital Marketing</h1>
+        <p class="text-lg">Donec sed odio dui consectetur adipiscing elit. Etiam adipiscing tincidunt elit, eu convallis <br> felis suscipit ut. Phasellus rhoncus tincidunt auctor. Nullam eu sagittis mauris at sapien.</p>
+        <hr>
+        <img src="https://assets-global.website-files.com/64f83b3ea875da04ea56a07f/64fedc045f8d9da37febafb9_gi4-large.svg" loading="lazy" alt="" class="feature-icon-image">
+        <h1 class="text-3xl">UX & Product Design</h1>
+        <p class="text-lg">Aenean tortor libero, condimentum ac laoreet vitae, varius tempor nisi. Duis non arcu vel <br> lectus urna mollis ornare vel leo varius blandit sit amet non magna eget risus.</p>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped>
-
+.sw{
+  position:relative;
+}
+h1 {
+    white-space: nowrap;
+}
+.navnul li{
+  margin-left:60px;
+}
+.caca{
+  transform:translate(-50%, -50%)translate(-80px, 10px);
+}
+.thick{
+  animation: moverlt 40s linear infinite;
+  overflow:hidden;
+  transform:translate(100%);
+}
+@keyframes moverlt{
+  from{
+    transform:translateX(0%);
+  }
+  to{
+    transform:translateX(-100%);
+  }
+}
 </style>
